ed648a436da91fd053ff8db331287bb937cf8e9b
Cameron <cameron.p.buckingham@gmail.com>
2016-01-18 19:43:12 -0700

    restarting a new session with custom home screen

--- MyApplication2/app/src/main/java/com/example/cpbuckingham/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/cpbuckingham/myapplication/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.cpbuckingham.firstapplication;
+package com.example.cpbuckingham.myapplication;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
