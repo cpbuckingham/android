5f88d7a2674be6140ff74bacfa320d368476d19c
Cameron <cameron.p.buckingham@gmail.com>
2016-01-24 19:40:20 -0600

    gestures

---
+++ Gestures2/app/src/main/java/com/example/cpbuckingham/gestures/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.cpbuckingham.gestures;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.GestureDetector;
+import android.support.v4.view.GestureDetectorCompat;
+
+
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    private TextView cams_message;
+    private GestureDetectorCompat gestureDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cams_message = (TextView)findViewById(R.id.cams_message);
+        this.gestureDetector = new GestureDetectorCompat(this,this);
+        gestureDetector.setOnDoubleTapListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        cams_message.setText("onSingleTap Confirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        cams_message.setText("onDoubleTap Confirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        cams_message.setText("onDoubleTapEvent Confirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        cams_message.setText("onDown Confirmed");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        cams_message.setText("onShowPress Confirmed");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        cams_message.setText("onSingleTapUp Confirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        cams_message.setText("onScroll Confirmed");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        cams_message.setText("onLongPress Confirmed");
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        cams_message.setText("onFling Confirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
