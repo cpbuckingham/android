cf621fc025b7684972e7bcc316af2c5595a3c8b8
Cameron <cpbuckingham@users.noreply.github.com>
2016-01-28 09:50:16 -0700

    topMemeText & bottomMemeText created

--- fragments/app/src/main/java/com/example/cpbuckingham/fragments/BottomPictureFragment.java
+++ fragments/app/src/main/java/com/example/cpbuckingham/fragments/BottomPictureFragment.java
@@ -6,12 +6,18 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BottomPictureFragment extends Fragment {
+    
+    private static TextView topMemeText;
+    private static Textview bottomMemeText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.bottom_picture_fragment, container, false);
+        topMemeText = (TextView) view.findViewById(R.id.topMemeText);
+        bottomMemeText = (TextView) view.findViewById(R.id.bottomMemeText);
         return view;
     }
 }
