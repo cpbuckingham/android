266ce912933b8f0cfb7003ab875eec84a274ff70
Cameron <cameron.p.buckingham@gmail.com>
2016-01-27 20:06:56 -0700

    buttonClicked method:

--- fragments/app/src/main/java/com/example/cpbuckingham/fragments/TopSectionFragment.java
+++ fragments/app/src/main/java/com/example/cpbuckingham/fragments/TopSectionFragment.java
@@ -6,13 +6,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 
 public class TopSectionFragment extends Fragment {
 
+    private static EditText topTextInput;
+    private static EditText bottomTextInput;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.top_section_fragment, container, false);
+
+        topTextInput = (EditText) view.findViewById(R.id.topTextInput);
+        bottomTextInput = (EditText) view.findViewById(R.id.bottomTextInput);
+        final Button button = (Button) view.findViewById(R.id.button);
+
+        button.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        buttonClicked(v);
+                    }
+                }
+        );
+
         return view;
+    }
+
+    public void buttonClicked(View view){
+        
     }
 }
