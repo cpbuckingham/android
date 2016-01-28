b150d3422cbe3dc26a60b3d462a3b0599d10cc58
Cameron <cameron.p.buckingham@gmail.com>
2016-01-28 08:46:11 -0700

    TopSectionListener added

--- fragments/app/src/main/java/com/example/cpbuckingham/fragments/TopSectionFragment.java
+++ fragments/app/src/main/java/com/example/cpbuckingham/fragments/TopSectionFragment.java
@@ -1,5 +1,6 @@
 package com.example.cpbuckingham.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -17,6 +18,21 @@ public class TopSectionFragment extends Fragment {
     private static EditText topTextInput;
     private static EditText bottomTextInput;
 
+    TopSectionListener activityCommander;
+
+    public interface TopSectionListener{
+        public void createMeme(String top, String bottom);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            activityCommander = (TopSectionListener) activity;
+        }catch(ClassCastException e ){
+            throw new ClassCastException(activity.toString());
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
@@ -36,8 +52,8 @@ public class TopSectionFragment extends Fragment {
 
         return view;
     }
-
+    // calls this when the button is clicked
     public void buttonClicked(View view){
-        
+        activityCommander.createMeme(topTextInput.getText().toString(), bottomTextInput.getText().toString());
     }
 }
