f6237215f7995569e8be1813c9ce3d0a9ed72b82
Cameron <cameron.p.buckingham@gmail.com>
2016-01-27 19:42:27 -0700

    bottom_section_fragment created

--- fragments/app/src/main/java/com/example/cpbuckingham/fragments/TopSectionFragment.java
+++ fragments/app/src/main/java/com/example/cpbuckingham/fragments/TopSectionFragment.java
@@ -1,7 +1,18 @@
 package com.example.cpbuckingham.fragments;
 
-/**
- * Created by cpbuckingham on 1/27/16.
- */
-public class TopSectionFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TopSectionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.top_section_fragment, container, false);
+        return view;
+    }
 }
