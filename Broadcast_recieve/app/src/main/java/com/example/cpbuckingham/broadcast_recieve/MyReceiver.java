abbb89e7e3d6a43c1fa04e23684f085f42f46bd8
Cam <cameron.p.buckingham@gmail.com>
2016-02-04 08:27:28 -0700

    receiving broadcasts

---
+++ Broadcast_recieve/app/src/main/java/com/example/cpbuckingham/broadcast_recieve/MyReceiver.java
@@ -0,0 +1,16 @@
+package com.example.cpbuckingham.broadcast_recieve;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "broadcast received", Toast.LENGTH_LONG).show();
+    }
+}
