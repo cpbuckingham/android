1c146be9bb01e375c2031f6e196f2ee5ef879b10
Cam <cameron.p.buckingham@gmail.com>
2016-03-05 09:31:59 -0600

    getTime: :

--- Bounded_services/app/src/main/java/com/example/cpbuckingham/bounded_services/MainActivity.java
+++ Bounded_services/app/src/main/java/com/example/cpbuckingham/bounded_services/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.content.Context;
 import android.os.IBinder;
 import android.content.Intent;
 import android.content.ComponentName;
@@ -21,12 +22,22 @@ public class MainActivity extends AppCompatActivity {
     MyService camsService;
     boolean isbound = false;
 
+    public void getTime(View view){
+        String currentTime = camsService.getCurrentTime();
+        TextView camsText = (TextView) findViewById(R.id.camsText);
+        camsText.setText(currentTime);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Intent i = new Intent(this, MyService.class);
+        bindService(i, camsConnection, Context.BIND_AUTO_CREATE);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -52,4 +63,21 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    private ServiceConnection camsConnection = new ServiceConnection(){
+        @Override
+            public void onServiceConnected(ComponentName name, IBinder service){
+                MyLocalBinder binder = (MyLocalBinder) service;
+                camsService = binder.getService();
+                isbound = true;
+
+        }
+            public void onServiceDisconnected(ComponentName name){
+                isbound = false;
+            }
+    };
+
+
 }
