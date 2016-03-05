a57e41b6bf1c29a7ad34dd26a81500754f67d97a
Cam <cameron.p.buckingham@gmail.com>
2016-03-05 09:09:33 -0600

    bounded service on mainActivity

--- Bounded_services/app/src/main/java/com/example/cpbuckingham/bounded_services/MainActivity.java
+++ Bounded_services/app/src/main/java/com/example/cpbuckingham/bounded_services/MainActivity.java
@@ -6,10 +6,20 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.os.IBinder;
+import android.content.Intent;
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import com.example.cpbuckingham.bounded_services.MyService.MyLocalBinder;
+
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyService camsService;
+    boolean isbound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +40,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
