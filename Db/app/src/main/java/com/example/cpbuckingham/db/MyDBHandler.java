09189b87331864c9fca261e47e3804586df9bb79
Cam <cameron.p.buckingham@gmail.com>
2016-03-12 17:25:10 -0600

    drop_table

--- Db/app/src/main/java/com/example/cpbuckingham/db/MyDBHandler.java
+++ Db/app/src/main/java/com/example/cpbuckingham/db/MyDBHandler.java
@@ -29,6 +29,9 @@ public class MyDBHandler extends SQLiteOpenHelper{
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_PRODUCTS);
+        onCreate(db);
     }
+
+    
 }
