d57f3dd66da6b438994e28dccd03f54826c5aaec
Cam <cameron.p.buckingham@gmail.com>
2016-03-21 16:51:29 -0500

    set up for notification with NotificationManager

---
+++ Notifications/app/src/main/java/com/example/cpbuckingham/notifications/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.cpbuckingham.notifications;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    NotificationCompat.Builder notification;
+    private static int uniqueID = 4938;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
