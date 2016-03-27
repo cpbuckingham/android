194b49aa826174b987a59b0c3bb3cf9fb699a2f9
Cam <cameron.p.buckingham@gmail.com>
2016-03-26 20:12:34 -0500

    shared preferences set up

---
+++ Shared_preferences/app/src/main/java/com/example/cpbuckingham/shared_preferences/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.cpbuckingham.shared_preferences;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    EditText passwordInput;
+    TextView camsText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        usernameInput = (EditText) findViewById(R.id.usernameInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        camsText = (TextView) findViewById(R.id.camsText);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
